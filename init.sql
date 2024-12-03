12759f28fbcb8f636a650a9571b62304bab6cbc7
Luiz Henrique <luizhn1703@gmail.com>
2024-12-03 00:37:37 -0300

    SQL initializated

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS SERVICES (
+    id INTEGER,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price REAL NOT NULL,
+    PRIMARY KEY (id)
+);
+                            
+CREATE TABLE IF NOT EXISTS SCHEDULING (
+    id INTEGER,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    data TEXT NOT NULL,
+    service_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (service_id) REFERENCES SERVICES(id)
+); 
